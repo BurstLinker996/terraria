24f50f6817f77e2c0659d9147be84926176d81b5
BurstLinker996 <panqihua@melt.icu>
2021-09-20 05:42:03 +0800

    修改tshock/Dockerfile里的镜像源为阿里镜像源，加快构建速度。 修改启动脚本tshock/bootstrap.sh，通过screen实现后台挂起mono启动的Terraria服务器。 增加docker-compose支持。

--- tshock/bootstrap.sh
+++ tshock/bootstrap.sh
@@ -25,14 +25,16 @@ if [ -z "$WORLD_FILENAME" ]; then
   else
     echo "Running server with command flags: $@"
   fi
-  mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" "$@" 
+  screen -dms TerrariaServer /bin/bash -c  'mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" "$@"'
 else
   echo "Environment WORLD_FILENAME specified"
   if [ -f "$WORLD_PATH" ]; then
     echo "Loading to world $WORLD_FILENAME..."
-    mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@" 
+    screen -dms TerrariaServer  /bin/bash -c  'mono --server --gc=sgen -O=all TerrariaServer.exe -configpath "$CONFIGPATH" -logpath "$LOGPATH" -world "$WORLD_PATH" "$@"'
   else
     echo "Unable to locate $WORLD_PATH.\nPlease make sure your world file is volumed into docker: -v <path_to_world_file>:/root/.local/share/Terraria/Worlds"
     exit 1
   fi
 fi
+screen -ls
+/bin/bash
